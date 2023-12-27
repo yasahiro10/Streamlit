2d70a952e597817f0e9f291d2f81699b459585e6
yasahiro10 <yassineouardani381@gmail.com>
2023-12-27 19:51:16 +0100

    tuto

--- Main.py
+++ Main.py
@@ -1,1 +1,5 @@
 import streamlit as st
+st.title("Hi i am streamlit web app")
+st.subheader("Hi I am your subheader")
+st.header("I am header")
+st.text("Hi i am text function and programmers uses")
