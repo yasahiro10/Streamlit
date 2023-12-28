a2d35c61a4ca70514d4c0de6e0bf8fbb81cca6a0
yasahiro10 <yassineouardani381@gmail.com>
2023-12-28 20:53:20 +0100

    text tutorial

--- Main.py
+++ Main.py
@@ -3,4 +3,15 @@ st.title("Hi i am streamlit web app")
 st.subheader("Hi I am your subheader")
 st.header("I am header")
 st.text("Hi i am text function and programmers uses")
-st.markdown("**HELLO** world")
+st.markdown("**HELLO** *world*")
+st.markdown(" ---")
+st.caption("hi i am caption")
+st.latex(r"\begin{pmatrix}a&b\\c&d\end{pmatrix}")
+json={"a":"1,2,3","b":"4,5,6"}
+st.json(json)
+code="""
+print("hello world")
+def funct():
+    return 0;
+"""
+st.code(code,language="python")
